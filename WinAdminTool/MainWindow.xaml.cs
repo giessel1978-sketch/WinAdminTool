811f5f2623fe62f08216bd4f51f70213979f7dfa
LordNikon <giessel1978@gmail.com>
2026-09-14 21:24:27 +0200

    App Shell - Navigation und Grundstruktur

--- WinAdminTool/MainWindow.xaml.cs
+++ WinAdminTool/MainWindow.xaml.cs
@@ -1,31 +1,33 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace WinAdminTool;
 
-namespace WinAdminTool
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        this.InitializeComponent();
+
+        MainNavigation.SelectedItem = MainNavigation.MenuItems[0];
+    }
+
+    private void MainNavigation_SelectionChanged(
+        NavigationView sender,
+        NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.SelectedItem is not NavigationViewItem selectedItem)
+            return;
+
+        string title = selectedItem.Content?.ToString() ?? "WinAdminTool";
+
+        ContentFrame.Content = new TextBlock
         {
-            InitializeComponent();
-        }
+            Text = title,
+            FontSize = 32,
+            FontWeight = Microsoft.UI.Text.FontWeights.SemiBold,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center
+        };
     }
 }
