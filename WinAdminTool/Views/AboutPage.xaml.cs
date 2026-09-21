b2ee8df249e1acc6e89ec1dd56083cf8589440aa
LordNikon <giessel1978@gmail.com>
2026-09-21 12:11:08 +0200

    csproj und appxmanifest geändert für Logo in Programm

--- WinAdminTool/Views/AboutPage.xaml.cs
+++ WinAdminTool/Views/AboutPage.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
+using System.Reflection;
 using Windows.System;
 
 namespace WinAdminTool.Views
@@ -12,11 +13,14 @@ namespace WinAdminTool.Views
         public AboutPage()
         {
             InitializeComponent();
+
+            VersionTextBlock.Text =
+                $"Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "unbekannt"} · Windows 10 / Windows 11 · x64";
         }
 
         private async void GitHubButton_Click(object sender, RoutedEventArgs e)
         {
-            await Launcher.LaunchUriAsync(new System.Uri(GitHubUrl));
+            await Launcher.LaunchUriAsync(new Uri(GitHubUrl));
         }
     }
 }
